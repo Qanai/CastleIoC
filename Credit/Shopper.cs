5075fef18a6094614c5f316f12853c81497263a9
Eliyahu Shlieserman <eliyahushli@gmail.com>
2016-02-07 21:41:29 +0200

    Initial Create

---
+++ Credit/Shopper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Credit
+{
+    public class Shopper
+    {
+        private readonly ICreditCard creditCard;
+
+        public int ChargesForCurrentCard
+        {
+            get { return creditCard.ChargeCount; }
+
+        }
+
+        public Shopper(ICreditCard creditCard)
+        {
+            this.creditCard = creditCard;
+        }
+
+        public string Charge()
+        {
+            return creditCard.Charge();
+        }
+    }
+}
